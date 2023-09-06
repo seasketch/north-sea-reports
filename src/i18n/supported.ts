891ce1038516b921208d8e248a2d426531619e47
Abby Vath Meyer <avmey@ucsb.edu>
2023-09-06 18:29:59 +0000

    Edits supported languages to English, Dutch, French, and Belgium French

--- src/i18n/supported.ts
+++ src/i18n/supported.ts
@@ -12,52 +12,10 @@ export interface LangDetails {
 const languages: LangDetails[] = [
   { name: "English", localName: "English", code: "EN" },
   ...[
-    { name: "Spanish", localName: "Español", code: "es" },
     {
-      name: "Dhivehi",
-      localName: "ދިވެހި,",
-      code: "dv",
-      rtl: true,
-    },
-    {
-      name: "Portuguese",
-      localName: "Portuguese",
-      code: "pt",
-    },
-    {
-      name: "Norwegian",
-      localName: "Norsk",
-      code: "no",
-    },
-    {
-      name: "Kosraean",
-      localName: "Kosraean",
-      code: "kos",
-    },
-    {
-      name: "Samoan",
-      localName: "Samoan",
-      code: "sm",
-    },
-    {
-      name: "Chuukese",
-      localName: "Chuukese",
-      code: "CHK",
-    },
-    {
-      name: "Fijian",
-      localName: "Na vosa vaka-Viti",
-      code: "fj",
-    },
-    {
-      name: "Fiji Hindi",
-      localName: "फ़िजी हिंदी",
-      code: "fh",
-    },
-    {
-      name: "Hawaiian",
-      localName: "ʻŌlelo Hawaiʻi",
-      code: "haw",
+      name: "Dutch",
+      code: "nl",
+      localName: "Nederlands",
     },
     {
       name: "French",
@@ -65,30 +23,9 @@ const languages: LangDetails[] = [
       code: "fr",
     },
     {
-      name: "German",
-      localName: "Deutsch",
-      code: "de",
-    },
-    {
-      name: "Croatian",
-      localName: "Hrvatski",
-      code: "hr",
-    },
-    {
-      name: "Afrikaans",
-      localName: "Afrikaans",
-      code: "af",
-    },
-    {
-      name: "Chinese",
-      localName: "中文",
-      code: "zh-Hans",
-    },
-    {
-      name: "Arabic",
-      localName: "اَلْعَرَبِيَّةُ",
-      code: "ar",
-      rtl: true,
+      code: "fr-be",
+      name: "French (Belgium)",
+      localName: "Français (Belgique)",
     },
   ].sort((a, b) => a.name.localeCompare(b.name)),
 ];
