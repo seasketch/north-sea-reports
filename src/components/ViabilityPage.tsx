11de7b804423d372ac2da149a213875844028d70
Abby Vath Meyer <avmey@ucsb.edu>
2023-09-07 20:41:51 +0000

    Update gp to match rename

--- src/components/ViabilityPage.tsx
+++ src/components/ViabilityPage.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { SizeCard } from "./SizeCard";
 import { SketchAttributesCard } from "@seasketch/geoprocessing/client-ui";
 import { GravelBeds } from "./GravelBeds";
-import { Reef } from "./Reef";
+import { Lanice } from "./Lanice";
 
 const ReportPage = () => {
   return (
     <>
       <SizeCard />
       <GravelBeds />
-      <Reef />
+      <Lanice />
       <SketchAttributesCard autoHide />
     </>
   );
